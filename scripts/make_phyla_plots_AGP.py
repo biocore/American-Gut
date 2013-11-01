58e71eb35d9ee3de2fe290817e9351cac12ee7a6
JWDebelius <j.debelius@gmail.com>
2013-11-01 12:38:28 -0600

    Corrected labels

--- scripts/make_phyla_plots_AGP.py
+++ scripts/make_phyla_plots_AGP.py
@@ -517,7 +517,8 @@ def make_phyla_plots_AGP(otu_table, mapping_data, categories, output_dir, \
             # Preallocates a numpy array for the plotting data
             tax_array = zeros((NUM_TAXA, NUM_CATS_TO_PLOT))        
             meta_data = map_dict[sample_id] 
-            cat_list = ['You', 'Average', 'Diet', 'BMI', 'Sex', 'Age', 
+            cat_list = ['You', 'Average', 'Similar Diet', ' Similar BMI', 
+                        'Same Gender', 'Similar Age', 
                         'Michael Pollan', '']
 
             #cat_list.append('Your Fecal Sample')
