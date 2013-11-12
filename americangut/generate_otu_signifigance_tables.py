2fb53b469dd8cfc9ef18262adcdba36c689ad21a
Daniel McDonald <mcdonadt@colorado.edu>
2013-11-12 16:54:44 -0700

    fold change is now rounded right

--- americangut/generate_otu_signifigance_tables.py
+++ americangut/generate_otu_signifigance_tables.py
@@ -131,7 +131,7 @@ def calculate_tax_rank_1(sample, population, taxa, critical_value = 0.05):
         list_value = [taxa[index], 
                       round(sample[index], 6), 
                          round(population_mean[index],6),
-                         round(ratio[index], 4), 
+                         round(ratio[index], 0), 
                          p_stat[index]]        
         if ratio[index] > 1:
             high.append(list_value)
