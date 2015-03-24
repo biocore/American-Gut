e03599a1716e71dc28be265e079eca27f566be39
Daniel McDonald <mcdonadt@colorado.edu>
2015-03-24 09:21:43 -0600

    TST: tests for the per sample bits

--- tests/test_taxtree.py
+++ tests/test_taxtree.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 
 from unittest import TestCase, main
-from americangut.taxtree import create_node, add_node, get_node, update_tree, \
-        get_rare_unique, traverse, sample_rare_unique
-from biom.table import table_factory
+
 from numpy import array
 from copy import deepcopy
+from biom import Table
+
+from americangut.taxtree import create_node, add_node, get_node, update_tree, \
+        get_rare_unique, traverse, sample_rare_unique, \
+        build_persample_tree_from_taxontable, set_relative_freqs, \
+        update_per_sample_tree
 
 __author__ = "Daniel McDonald"
 __copyright__ = "Copyright 2013, The American Gut Project"
@@ -16,38 +20,124 @@ __maintainer__ = "Daniel McDonald"
 __email__ = "mcdonadt@colorado.edu"
 
 class TaxTreeTests(TestCase):
+    def setUp(self):
+        ta = {'name': 'root',
+              'count': 36,
+              'freq': 1.0,
+              'children': [{'name': 'k__1',
+                            'count': 36,
+                            'freq': 1.0,
+                            'children': [{'name': 'p__x',
+                                          'count': 12,
+                                          'freq': 12.0 / 36.0,
+                                          'children': [{'name': 'c__1',
+                                                        'count': 1,
+                                                        'freq': 1.0 / 36,
+                                                        'children': []},
+                                                       {'name': 'c__2',
+                                                        'count': 4,
+                                                        'freq': 4.0 / 36,
+                                                        'children': []},
+                                                       {'name': 'c__',
+                                                        'count': 7,
+                                                        'freq': 7.0 / 36,
+                                                        'children': []}]},
+                                         {'name': 'p__y',
+                                          'count': 24,
+                                          'freq': 24.0 / 36.0,
+                                          'children': [{'name': 'c__3',
+                                                        'count': 10,
+                                                        'freq': 10.0 / 36.0,
+                                                        'children': []},
+                                                       {'name': 'c__',
+                                                        'count': 14,
+                                                        'freq': 14.0 / 36.0,
+                                                        'children': []}]}]}]}
+        self.persample = ta
+
+    def test_build_persample_tree_from_taxontable(self):
+        exp_a = ('a', self.persample)
+        obs_a = build_persample_tree_from_taxontable(table).next()
+        self.assertEqual(obs_a, exp_a)
+
+    def test_set_relative_freqs(self):
+        persample_copy = deepcopy(self.persample)
+        self.persample['freq'] = None
+        for node in traverse(self.persample):
+            node['freq'] = None
+        set_relative_freqs(self.persample)
+        self.assertEqual(persample_copy, self.persample)
+
+    def test_update_per_sample_tree(self):
+        taxons = (('k__a', 'p__x'),
+                  ('k__a', 'p__x'),
+                  ('k__a', 'p__y'),  # indep of p__x; c__1
+                  ('k__b', 'p__z'))
+        counts = (5, 7, 9, 11)
+        exp = {'name': 'root',
+               'count': 32,
+               'freq': None,
+               'children': [{'name': 'k__a',
+                             'count': 21,
+                             'freq': None,
+                             'children': [{'name': 'p__x',
+                                           'count': 12,
+                                           'freq': None,
+                                           'children': []},
+                                          {'name': 'p__y',
+                                           'count': 9,
+                                           'freq': None,
+                                           'children': []}]},
+                            {'name': 'k__b',
+                             'count': 11,
+                             'freq': None,
+                             'children': [{'name': 'p__z',
+                                           'count': 11,
+                                           'freq': None,
+                                           'children': []}]}]}
+
+        tree = create_node('root', count=0, freq=None)
+        update_per_sample_tree(tree, taxons[0], counts[0])
+        update_per_sample_tree(tree, taxons[1], counts[1])
+        update_per_sample_tree(tree, taxons[2], counts[2])
+        update_per_sample_tree(tree, taxons[3], counts[3])
+        self.assertEqual(tree, exp)
+
     def test_sample_rare_unique(self):
         t = update_tree(None, tax_strings_by_sample)
         tax_by_sample = {'a':tax_strings_by_sample[0],
                          'b':tax_strings_by_sample[1],
                          'c':tax_strings_by_sample[2]}
-        exp = [('a', None, [['k__1','p__x','c__'],['k__1','p__y','c__3']], 
+        exp = [('a', None, [['k__1','p__x','c__'],['k__1','p__y','c__3']],
                            [['k__1','p__x','c__1'],['k__1','p__x','c__2']]),
                ('b', None, [['k__1','p__x','c__'],['k__1','p__y','c__3']], []),
                ('c', None, [], [])]
         obs = sample_rare_unique(t, None, tax_by_sample, 0.7)
         self.assertEqual(sorted(obs), exp)
 
-        table_a = table_factory(array([[14,15,16]]), ['a','b','c'], 
-                                ['k__1; p__y; c__'])
-        table_b = table_factory(array([[1,2,3],
-                                       [4,5,6],
-                                       [14,15,16]]), ['a','b','c'], 
-                                    ['k__1; p__x; c__1',
-                                     'k__1; p__x; c__2',
-                                     'k__1; p__y; c__'])
-        table_c = table_factory(array([[1,2,3],
-                                       [4,5,6],
-                                       [7,8,9],
-                                       [10,11,12],
-                                       [14,15,16]]), ['a','b','c'], 
-                                    ['k__1; p__x; c__1',
-                                     'k__1; p__x; c__2',
-                                     'k__1; p__x; c__',
-                                     'k__1; p__y; c__3',
-                                     'k__1; p__y; c__'])
+        table_a = Table(array([[14,15,16]]),
+                               ['k__1; p__y; c__'],
+                               ['a','b','c'])
+        table_b = Table(array([[1,2,3],
+                               [4,5,6],
+                               [14,15,16]]),
+                        ['k__1; p__x; c__1',
+                         'k__1; p__x; c__2',
+                         'k__1; p__y; c__'],
+                        ['a','b','c'], )
+        table_c = Table(array([[1,2,3],
+                               [4,5,6],
+                               [7,8,9],
+                               [10,11,12],
+                               [14,15,16]]),
+                        ['k__1; p__x; c__1',
+                         'k__1; p__x; c__2',
+                         'k__1; p__x; c__',
+                         'k__1; p__y; c__3',
+                         'k__1; p__y; c__'],
+                        ['a','b','c'])
 
-        exp = [('a', table_a, [['k__1','p__x','c__'],['k__1','p__y','c__3']], 
+        exp = [('a', table_a, [['k__1','p__x','c__'],['k__1','p__y','c__3']],
                            [['k__1','p__x','c__1'],['k__1','p__x','c__2']]),
                ('b', table_b, [['k__1','p__x','c__'],['k__1','p__y','c__3']], []),
                ('c', table_c, [], [])]
@@ -60,7 +150,7 @@ class TaxTreeTests(TestCase):
             self.assertEqual(o[3], e[3])
 
     def test_create_node(self):
-        exp = {'name':'foo','popcount':0,'children':[]}
+        exp = {'name':'foo','children':[]}
         obs = create_node('foo')
         self.assertEqual(obs, exp)
 
@@ -71,12 +161,12 @@ class TaxTreeTests(TestCase):
         self.assertEqual(obs, exp)
 
     def test_add_node(self):
-        exp = {'name':'foo','popcount':0,'children':[
-                        {'name':'bar', 'popcount':0, 'children':[]}
+        exp = {'name':'foo','popcount':1,'children':[
+                        {'name':'bar', 'popcount':2, 'children':[]}
                      ]
               }
-        foo = create_node('foo')
-        bar = create_node('bar')
+        foo = create_node('foo', popcount=1)
+        bar = create_node('bar', popcount=2)
         add_node(foo,bar)
         self.assertEqual(foo, exp)
 
@@ -86,23 +176,23 @@ class TaxTreeTests(TestCase):
                     ]}
         self.assertEqual(get_node(tree, 'does not exist'), None)
         self.assertEqual(get_node(tree, 'bar'), tree['children'][0])
-    
+
     def test_create_tree(self):
         """take tax strings, create a tree"""
         exp = {'name':'root',
-                'popcount':3, 
+                'popcount':3,
                 'children':[
                     {'name':'k__1',
                      'popcount':3,
                      'children':[
-                        {'name':'p__x', 
+                        {'name':'p__x',
                          'popcount':2,
                          'children':[
-                            {'name':'c__1', 
-                             'popcount':1, 
+                            {'name':'c__1',
+                             'popcount':1,
                              'children':[]},
-                            {'name':'c__2', 
-                             'popcount':1, 
+                            {'name':'c__2',
+                             'popcount':1,
                              'children':[]}
                             ]
                         },
@@ -124,19 +214,19 @@ class TaxTreeTests(TestCase):
     def test_create_tree_ignore_contested(self):
         """take tax strings, create a tree"""
         exp = {'name':'root',
-                'popcount':3, 
+                'popcount':3,
                 'children':[
                     {'name':'k__1',
                      'popcount':3,
                      'children':[
-                        {'name':'p__x', 
+                        {'name':'p__x',
                          'popcount':2,
                          'children':[
-                            {'name':'c__1', 
-                             'popcount':1, 
+                            {'name':'c__1',
+                             'popcount':1,
                              'children':[]},
-                            {'name':'c__2', 
-                             'popcount':1, 
+                            {'name':'c__2',
+                             'popcount':1,
                              'children':[]}
                             ]
                         },
@@ -157,7 +247,7 @@ class TaxTreeTests(TestCase):
         exp_unique = [['k__1','p__x','c__2']]
         exp_rare = [['k__1','p__x','c__'],['k__1','p__y','c__3']]
         obs_rare, obs_unique = get_rare_unique(t, sample_taxa, 0.7)
-        
+
         self.assertEqual(obs_rare, exp_rare)
         self.assertEqual(obs_unique, exp_unique)
 
@@ -184,20 +274,24 @@ tax_strings_by_sample = [
         ]
     ]
 
-table = table_factory(array([[1,2,3],
-           [4,5,6],
-           [7,8,9],
-           [10,11,12],
-           [14,15,16]]), ['a','b','c'],['k__1; p__x; c__1', 
-                                        'k__1; p__x; c__2', 
-                                        'k__1; p__x; c__',
-                                        'k__1; p__y; c__3',
-                                        'k__1; p__y; c__'])
+table = Table(array([[1,2,3],
+                     [4,5,6],
+                     [7,8,9],
+                     [10,11,12],
+                     [14,15,16]]),
+              ['k__1; p__x; c__1',
+               'k__1; p__x; c__2',
+               'k__1; p__x; c__',
+               'k__1; p__y; c__3',
+               'k__1; p__y; c__'],
+              ['a','b','c'])
 
 sample_taxa = [
         ['k__1','p__x','c__2'],
         ['k__1','p__x','c__'],
         ['k__1','p__y','c__3']
     ]
+
+
 if __name__ == '__main__':
     main()
