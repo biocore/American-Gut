e79d736305bd793b690c1dd8885ef87a5de98951
Luke Thompson <lukethompson@gmail.com>
2015-05-18 17:23:13 -0700

    Fixed zorder and coloring

--- scripts/mod2_pcoa.py
+++ scripts/mod2_pcoa.py
@@ -7,108 +7,130 @@ import pandas as pd
 import seaborn as sns
 from skbio.stats.ordination import OrdinationResults
 from collections import defaultdict
+from collections import OrderedDict
 
 ALPHA = 1.0
 LINE_WIDTH = 0.3
 
+
 @click.group()
-def mod1_pcoa():
+def mod2_pcoa():
     pass
 
-@mod1_pcoa.command()
-@click.option('--coords', required=True, type=click.Path(resolve_path=True, readable=True, exists=True), help='Coordinates file')
-@click.option('--mapping_file', required=True, type=click.Path(resolve_path=True, readable=True, exists=True), help='Mapping file')
-def body_site(coords,mapping_file):
-    """Generates as many figures as samples in the coordinates file (Figure 1)"""
+
+@mod2_pcoa.command()
+@click.option('--coords', required=True, type=click.Path(
+              resolve_path=True, readable=True, exists=True),
+              help='Coordinates file')
+@click.option('--mapping_file', required=True, type=click.Path(
+              resolve_path=True, readable=True, exists=True),
+              help='Mapping file')
+def body_site(coords, mapping_file):
+    """Generates as many figures as samples in the coordinates file"""
     o = OrdinationResults.from_file(coords)
-    x, y = o.site[:,0], o.site[:,1]
 
     # coordinates
     c_df = pd.DataFrame(o.site, o.site_ids)
 
     # mapping file
-    mf = pd.read_csv(mapping_file, '\t', converters=defaultdict(str), index_col='#SampleID')
+    mf = pd.read_csv(mapping_file, '\t', converters=defaultdict(str),
+                     index_col='#SampleID')
     mf = mf.loc[o.site_ids]
 
-    color_hmp_fecal = sns.color_palette('Paired', 12)[10] # light brown
-    color_agp_fecal = sns.color_palette('Paired', 12)[11] # dark brown
-    color_hmp_oral  = sns.color_palette('Paired', 12)[0]  # light blue
-    color_agp_oral  = sns.color_palette('Paired', 12)[1]  # dark blue
-    color_hmp_skin  = sns.color_palette('Paired', 12)[2]  # light green
-    color_agp_skin  = sns.color_palette('Paired', 12)[3]  # dark green
+    color_hmp_fecal = sns.color_palette('Paired', 12)[10]  # light brown
+    color_agp_fecal = sns.color_palette('Paired', 12)[11]  # dark brown
+    color_hmp_oral = sns.color_palette('Paired', 12)[0]    # light blue
+    color_agp_oral = sns.color_palette('Paired', 12)[1]    # dark blue
+    color_hmp_skin = sns.color_palette('Paired', 12)[2]    # light green
+    color_agp_skin = sns.color_palette('Paired', 12)[3]    # dark green
 
-    cat_colors = {'HMP-FECAL': color_hmp_fecal,
+    grp_colors = {'AGP-FECAL': color_agp_fecal,
+                  'AGP-ORAL':  color_agp_oral,
+                  'AGP-SKIN':  color_agp_skin,
+                  'HMP-FECAL': color_hmp_fecal,
                   'GG-FECAL':  color_hmp_fecal,
-                  'PGP-FECAL': color_hmp_fecal, 
-                  'AGP-FECAL': color_agp_fecal, 
+                  'PGP-FECAL': color_hmp_fecal,
                   'HMP-ORAL':  color_hmp_oral,
                   'PGP-ORAL':  color_hmp_oral,
-                  'AGP-ORAL':  color_agp_oral, 
                   'HMP-SKIN':  color_hmp_skin,
-                  'PGP-SKIN':  color_hmp_skin,
-                  'AGP-SKIN':  color_agp_skin}
+                  'PGP-SKIN':  color_hmp_skin}
 
     for sample in mf.index:
-        for cat, color in cat_colors.iteritems():
-            sub_coords = c_df[mf.TITLE_BODY_SITE == cat]
 
-            plt.scatter(sub_coords[0], sub_coords[1], color=color, edgecolor='k', lw=LINE_WIDTH, alpha=ALPHA)
+        # plot categories as 50 slices with random zorder
+        for grp, color in grp_colors.iteritems():
+            sub_coords = c_df[mf.TITLE_BODY_SITE == grp].values
+            for i in np.array_split(sub_coords, 50):
+                plt.scatter(i[:, 0], i[:, 1], color=color,
+                            edgecolor=np.asarray(color)*0.6, lw=LINE_WIDTH,
+                            alpha=ALPHA, zorder=np.random.rand())
 
+        # plot participant's dot
         plt.scatter(c_df.loc[sample][0], c_df.loc[sample][1],
-                    color=cat_colors[mf.loc[sample]['TITLE_BODY_SITE']],
-                    s=270, edgecolor='w')
+                    color=grp_colors[mf.loc[sample]['TITLE_BODY_SITE']],
+                    s=270, edgecolor='w', zorder=1)
         plt.scatter(c_df.loc[sample][0], c_df.loc[sample][1],
-                    color=cat_colors[mf.loc[sample]['TITLE_BODY_SITE']],
-                    s=250, edgecolor='k')
+                    color=grp_colors[mf.loc[sample]['TITLE_BODY_SITE']],
+                    s=250, edgecolor=np.asarray(
+                    grp_colors[mf.loc[sample]['TITLE_BODY_SITE']])*0.6,
+                    zorder=2)
+
         plt.axis('off')
         my_dpi = 72
-        plt.savefig(sample+'.pdf', figsize=(1000/my_dpi, 1000/my_dpi), dpi=my_dpi)
+        plt.savefig(sample+'.pdf', figsize=(1000/my_dpi, 1000/my_dpi),
+                    dpi=my_dpi)
         plt.close()
 
-@mod1_pcoa.command()
-@click.option('--coords', required=True, type=click.Path(resolve_path=True, readable=True, exists=True), help='Coordinates file')
-@click.option('--mapping_file', required=True, type=click.Path(resolve_path=True, readable=True, exists=True), help='Mapping file')
-def country(coords,mapping_file):
-    """Generates as many figures as samples in the coordinates file (Figure 1)"""
+
+@mod2_pcoa.command()
+@click.option('--coords', required=True, type=click.Path(resolve_path=True,
+              readable=True, exists=True), help='Coordinates file')
+@click.option('--mapping_file', required=True, type=click.Path(
+              resolve_path=True, readable=True, exists=True),
+              help='Mapping file')
+def country(coords, mapping_file):
+    """Generates as many figures as samples in the coordinates file"""
     o = OrdinationResults.from_file(coords)
-    x, y = o.site[:,0], o.site[:,1]
+    x, y = o.site[:, 0], o.site[:, 1]
 
     # coordinates
     c_df = pd.DataFrame(o.site, o.site_ids)
 
     # mapping file
-    mf = pd.read_csv(mapping_file, '\t', converters=defaultdict(str), index_col='#SampleID')
+    mf = pd.read_csv(mapping_file, '\t', converters=defaultdict(str),
+                     index_col='#SampleID')
     mf = mf.loc[o.site_ids]
 
     color_Venezuela = sns.color_palette('Paired', 12)[10]
-    color_Malawi    = sns.color_palette('Paired', 12)[1]
-    color_Western   = sns.color_palette('Paired', 12)[4]
+    color_Malawi = sns.color_palette('Paired', 12)[1]
+    color_Western = sns.color_palette('Paired', 12)[4]
     color_Highlight = sns.color_palette('Paired', 12)[5]
-    color_no_data   = '0.5'
+    color_no_data = (0.5, 0.5, 0.5)
 
-    cat_colors = {'Australia':                color_Western,
-                  'Belgium':                  color_Western,
-                  'Canada':                   color_Western,
-                  'China':                    color_Western,
-                  'Finland':                  color_Western,
-                  'France':                   color_Western,
-                  'Germany':                  color_Western,
-                  'Great Britain':            color_Western,
-                  'Ireland':                  color_Western,
-                  'Japan':                    color_Western,
-                  'Malawi':                   color_Malawi,
-                  'Netherlands':              color_Western,
-                  'New Zealand':              color_Western,
-                  'Norway':                   color_Western,
-                  'Scotland':                 color_Western,
-                  'Spain':                    color_Western,
-                  'Switzerland':              color_Western,
-                  'Thailand':                 color_Western,
-                  'United Arab Emirates':     color_Western,
-                  'United Kingdom':           color_Western,
-                  'United States of America': color_Western,
-                  'Venezuela':                color_Venezuela,
-                  'no_data':                  color_Western}
+    grp_colors = OrderedDict()
+    grp_colors['no_data'] = color_no_data
+    grp_colors['Australia'] = color_Western
+    grp_colors['Belgium'] = color_Western
+    grp_colors['Canada'] = color_Western
+    grp_colors['China'] = color_Western
+    grp_colors['Finland'] = color_Western
+    grp_colors['France'] = color_Western
+    grp_colors['Germany'] = color_Western
+    grp_colors['Great Britain'] = color_Western
+    grp_colors['Ireland'] = color_Western
+    grp_colors['Japan'] = color_Western
+    grp_colors['Netherlands'] = color_Western
+    grp_colors['New Zealand'] = color_Western
+    grp_colors['Norway'] = color_Western
+    grp_colors['Scotland'] = color_Western
+    grp_colors['Spain'] = color_Western
+    grp_colors['Switzerland'] = color_Western
+    grp_colors['Thailand'] = color_Western
+    grp_colors['United Arab Emirates'] = color_Western
+    grp_colors['United Kingdom'] = color_Western
+    grp_colors['United States of America'] = color_Western
+    grp_colors['Malawi'] = color_Malawi
+    grp_colors['Venezuela'] = color_Venezuela
 
     for sample in mf.index:
 
@@ -116,43 +138,69 @@ def country(coords,mapping_file):
         sns.kdeplot(x, y, cmap='bone')
         sns.set_context(rc={"lines.linewidth": 0.75})
 
-        # change particapant's country's color to color_Highlight unless country is Venezuela or Malawi
-        if (mf.loc[sample]['COUNTRY'] != 'Malawi') & (mf.loc[sample]['COUNTRY'] != 'Venezuela'):
-            cat_colors[mf.loc[sample]['COUNTRY']] = color_Highlight
+        # change particapant's country's color to color_Highlight unless
+        # country is Venezuela or Malawi
+        if (mf.loc[sample]['COUNTRY'] != 'Malawi') & (mf.loc[sample]['COUNTRY']
+                                                      != 'Venezuela'):
+            grp_colors[mf.loc[sample]['COUNTRY']] = color_Highlight
 
-        # plot each country according to colors above
-        for cat, color in cat_colors.iteritems():
-            sub_coords = c_df[mf.COUNTRY == cat]
-            plt.scatter(sub_coords[0], sub_coords[1], color=color, edgecolor='k', lw=LINE_WIDTH, alpha=ALPHA)
+        # plot each country except participant's according to colors above
+        for grp, color in grp_colors.iteritems():
+            if grp == mf.loc[sample]['COUNTRY']:
+                continue
+            sub_coords = c_df[mf.COUNTRY == grp]
+            plt.scatter(sub_coords[0], sub_coords[1], color=color,
+                        edgecolor=np.asarray(color)*0.6, lw=LINE_WIDTH,
+                        alpha=ALPHA)
+
+        # now plot participant's country
+        grp = mf.loc[sample]['COUNTRY']
+        color = grp_colors[grp]
+        sub_coords = c_df[mf.COUNTRY == grp]
+        plt.scatter(sub_coords[0], sub_coords[1], color=color,
+                    edgecolor=np.asarray(color)*0.6, lw=LINE_WIDTH,
+                    alpha=ALPHA)
 
         # plot participant's dot
         plt.scatter(c_df.loc[sample][0], c_df.loc[sample][1],
-                    color=cat_colors[mf.loc[sample]['COUNTRY']],
+                    color=grp_colors[mf.loc[sample]['COUNTRY']],
                     s=270, edgecolor='w', zorder=1)
         plt.scatter(c_df.loc[sample][0], c_df.loc[sample][1],
-                    color=cat_colors[mf.loc[sample]['COUNTRY']],
-                    s=250, edgecolor='k', zorder=2)      
-        
-        # reset particapant's country's color to color_Western unless country is Venezuela or Malawi
-        if (mf.loc[sample]['COUNTRY'] != 'Malawi') & (mf.loc[sample]['COUNTRY'] != 'Venezuela'):
-            cat_colors[mf.loc[sample]['COUNTRY']] = color_Western
+                    color=grp_colors[mf.loc[sample]['COUNTRY']],
+                    s=250, edgecolor=np.asarray(grp_colors[mf.loc[sample]
+                                                ['COUNTRY']])*0.6, zorder=2)
+
+        # reset particapant's country's color to color_Western unless country
+        # is Venezuela or Malawi
+        if (mf.loc[sample]['COUNTRY'] != 'Malawi') & (mf.loc[sample]['COUNTRY']
+                                                      != 'Venezuela'):
+            grp_colors[mf.loc[sample]['COUNTRY']] = color_Western
 
         plt.axis('off')
         my_dpi = 72
-        plt.savefig(sample+'.pdf', figsize=(1000/my_dpi, 1000/my_dpi), dpi=my_dpi)
+        plt.savefig(sample+'.pdf', figsize=(1000/my_dpi, 1000/my_dpi),
+                    dpi=my_dpi)
         plt.close()
 
-@mod1_pcoa.command()
-@click.option('--coords', required=True, type=click.Path(resolve_path=True, readable=True, exists=True), help='Coordinates file')
-@click.option('--mapping_file', required=True, type=click.Path(resolve_path=True, readable=True, exists=True), help='Mapping file')
-@click.option('--color', required=True, type=str, help='Metadata category to set color by')
-def gradient(coords,mapping_file, color):
-    """Generates as many figures as samples in the coordinates file (Figures 2 & 3)"""
+
+@mod2_pcoa.command()
+@click.option('--coords', required=True, type=click.Path(resolve_path=True,
+              readable=True, exists=True), help='Coordinates file')
+@click.option('--mapping_file', required=True, type=click.Path(
+              resolve_path=True, readable=True, exists=True),
+              help='Mapping file')
+@click.option('--color', required=True, type=str,
+              help='Metadata category to set color by')
+def gradient(coords, mapping_file, color):
+    """Generates as many figures as samples in the coordinates file"""
     o = OrdinationResults.from_file(coords)
+
+    # coordinates
     c_df = pd.DataFrame(o.site, o.site_ids)
 
     # mapping file
-    mf = pd.read_csv(mapping_file, '\t', converters=defaultdict(str), index_col='#SampleID')
+    mf = pd.read_csv(mapping_file, '\t', converters=defaultdict(str),
+                     index_col='#SampleID')
     mf = mf.loc[o.site_ids]
     mf[color] = mf[color].convert_objects(convert_numeric=True)
 
@@ -163,114 +211,40 @@ def gradient(coords,mapping_file, color):
 
     for sample in mf.index:
 
-        # NUMERIC
+        # plot numeric metadata as colored gradient
         ids = numeric.index
         x, y = c_df.loc[ids][0], c_df.loc[ids][1]
         plt.scatter(x, y, c=numeric[color], cmap=plt.get_cmap('RdBu'),
-                    alpha=ALPHA, lw=LINE_WIDTH)
+                    alpha=ALPHA, lw=LINE_WIDTH, edgecolor=color_array*0.6)
 
-        #plt.colorbar()
+        # plt.colorbar()
 
-        # NON-NUMERIC
+        # plot non-numeric metadata as gray
         ids = non_numeric.index
         x, y = c_df.loc[ids][0], c_df.loc[ids][1]
-        plt.scatter(x, y, c='0.5', alpha=ALPHA, lw=LINE_WIDTH)
+        plt.scatter(x, y, c='0.5', alpha=ALPHA, lw=LINE_WIDTH, edgecolor='0.3')
 
-        # INDIVIDUAL BIG DOT
+        # plot individual's dot
         try:
             color_index = numeric.index.tolist().index(sample)
         except ValueError:
             color_index = None
 
         if color_index is None:
-            _color = '0.5'
+            _color = (0.5, 0.5, 0.5)
         else:
             _color = color_array[color_index]
 
         plt.scatter(c_df.loc[sample][0], c_df.loc[sample][1],
-            color=_color,
-            s=270, edgecolor='w')
+                    color=_color, s=270, edgecolor='w')
         plt.scatter(c_df.loc[sample][0], c_df.loc[sample][1],
-            color=_color,
-            s=250, edgecolor='k')
+                    color=_color, s=250, edgecolor=np.asarray(_color)*0.6)
 
         plt.axis('off')
         my_dpi = 72
-        plt.savefig(sample+'.pdf', figsize=(1000/my_dpi, 1000/my_dpi), dpi=my_dpi)
-        plt.close()
-
-@mod1_pcoa.command()
-@click.option('--coords', required=True, type=click.Path(resolve_path=True, readable=True, exists=True), help='Coordinates file')
-@click.option('--mapping_file', required=True, type=click.Path(resolve_path=True, readable=True, exists=True), help='Mapping file')
-@click.option('--color', required=True, type=str, help='Metadata category to set color by')
-def double_gradient(coords,mapping_file, color):
-    """Generates as many figures as samples in the coordinates file (Figures 2 & 3)"""
-    o = OrdinationResults.from_file(coords)
-    c_df = pd.DataFrame(o.site, o.site_ids)
-
-    # mapping file
-    mf = pd.read_csv(mapping_file, '\t', converters=defaultdict(str), index_col='#SampleID')
-    mf = mf.loc[o.site_ids]
-    mf[color] = mf[color].convert_objects(convert_numeric=True)
-
-    numeric = mf[~pd.isnull(mf[color])]
-    non_numeric = mf[pd.isnull(mf[color])]
-
-    numeric_UK = numeric[numeric.COUNTRY == 'United Kingdom']
-    numeric_Other = numeric[numeric.COUNTRY != 'United Kingdom']
-
-    color_array_UK = plt.cm.spring(numeric_UK[color]/max(numeric_UK[color]))
-    color_array_Other = plt.cm.winter(numeric_Other[color]/max(numeric_Other[color]))
-
-    for sample in mf.index:
-
-        # NUMERIC OTHER
-        ids = numeric_Other.index
-        x, y = c_df.loc[ids][0], c_df.loc[ids][1]
-        plt.scatter(x, y, c=numeric_Other[color], cmap=plt.get_cmap('winter'),
-                    alpha=ALPHA, lw=LINE_WIDTH)
-
-        # NUMERIC UK
-        ids = numeric_UK.index
-        x, y = c_df.loc[ids][0], c_df.loc[ids][1]
-        plt.scatter(x, y, c=numeric_UK[color], cmap=plt.get_cmap('spring'),
-                    alpha=ALPHA, lw=LINE_WIDTH)
-
-        #plt.colorbar()
-
-        # NON-NUMERIC
-        ids = non_numeric.index
-        x, y = c_df.loc[ids][0], c_df.loc[ids][1]
-        plt.scatter(x, y, c='0.5', alpha=ALPHA, lw=LINE_WIDTH)
-
-        # INDIVIDUAL BIG DOT
-        try:
-            if mf.loc[sample].COUNTRY == 'United Kingdom':
-                color_index = numeric_UK.index.tolist().index(sample)
-            else:
-                color_index = numeric_Other.index.tolist().index(sample)
-        except ValueError:
-            color_index = None
-
-        if color_index is None:
-            _color = '0.5'
-        else:
-            if mf.loc[sample].COUNTRY == 'United Kingdom':
-                _color = color_array_UK[color_index]
-            else:
-                _color = color_array_Other[color_index]
-
-        plt.scatter(c_df.loc[sample][0], c_df.loc[sample][1],
-            color=_color,
-            s=270, edgecolor='w')
-        plt.scatter(c_df.loc[sample][0], c_df.loc[sample][1],
-            color=_color,
-            s=250, edgecolor='k')
-
-        plt.axis('off')
-        my_dpi = 72
-        plt.savefig(sample+'.pdf', figsize=(1000/my_dpi, 1000/my_dpi), dpi=my_dpi)
+        plt.savefig(sample+'.pdf', figsize=(1000/my_dpi, 1000/my_dpi),
+                    dpi=my_dpi)
         plt.close()
 
 if __name__ == '__main__':
-    mod1_pcoa()
+    mod2_pcoa()
