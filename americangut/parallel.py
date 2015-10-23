45150dd4009782fdfbcedf5c40793db910340064
Daniel McDonald <mcdonadt@colorado.edu>
2015-10-22 21:22:07 -0600

    other lib code

---
+++ americangut/parallel.py
@@ -0,0 +1,31 @@
+import multiprocessing as mp
+
+import americangut.results_utils as agru
+
+
+def dispatcher(success_fp, fail_fp, partitions):
+    """Dispatch execution over a pool of processors
+
+    Parameters
+    ----------
+    success_fp : file-like object
+        A file-like object to write a list of successful sample IDs too
+    fail_fp : file-like object
+        A file-like object to write a list of unsuccessful sample IDs too,
+        and any associated error information
+    partitions : iterable
+        Yields a function and an iterable of IDs. It is expected that this
+        function will return a dict of {str: list}.
+    """
+    pool = mp.Pool(processes=mp.cpu_count())
+
+    success_fp.write('%s\n' % '#SampleID')
+    fail_fp.write('%s\t%s\n' % ('#SampleID', 'Error(s)'))
+
+    for func, ids in partitions:
+        for success_details in pool.map(func, list(agru.chunk_list(ids))):
+            for id_, detail in success_details.items():
+                if detail:
+                    fail_fp.write("%s\t%s\n" % (id_, '\t'.join(detail)))
+                else:
+                    success_fp.write("%s\n" % id_)
